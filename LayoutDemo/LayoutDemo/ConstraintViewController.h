bc2e3df22090d137ce4a4189e3b2554f3424cd55
qiaohaibin <qiaohaibin@xiaomi.com>
2017-02-24 18:47:30 +0800

    auto_layout_guide_1

---
+++ LayoutDemo/LayoutDemo/ConstraintViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConstraintViewController.h
+//  LayoutDemo
+//
+//  Created by qiaohaibin on 2/24/17.
+//  Copyright © 2017 xiaomi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConstraintViewController : UIViewController
+
+@end
---
+++ LayoutDemo/LayoutDemo/StackViewController.h
@@ -0,0 +1,13 @@
+//
+//  StackViewController.h
+//  LayoutDemo
+//
+//  Created by qiaohaibin on 2/24/17.
+//  Copyright © 2017 xiaomi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StackViewController : UIViewController
+
+@end
